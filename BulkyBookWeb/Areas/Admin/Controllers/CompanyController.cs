c61c57a7e1c91f79ada614f8511db560317228f6
Abhishek Patel <abhipatel7420@gmail.com>
2022-11-01 08:36:34 +0530

    implement role base authorization

--- BulkyBookWeb/Areas/Admin/Controllers/CompanyController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -1,6 +1,8 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
 using BulkyBook.Models.ViewModels;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +10,7 @@ using Microsoft.Extensions.Hosting;
 namespace BulkyBookWeb.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = StaticDetails.Role_Admin)]
     public class CompanyController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
