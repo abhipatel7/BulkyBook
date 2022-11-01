bc565641a80b4f3ed30b35a6fc319c2ab67dccbe
Abhishek Patel <abhipatel7420@gmail.com>
2022-11-01 09:22:46 +0530

    implement sendgrid

--- BulkyBook.Utility/EmailSender.cs
+++ BulkyBook.Utility/EmailSender.cs
@@ -1,6 +1,9 @@
 ﻿using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
 using MimeKit;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,24 +14,36 @@ namespace BulkyBook.Utility
 {
     public class EmailSender : IEmailSender
     {
+        public string SendGridSecret { get; set; }
+        public string SendGridEmail { get; set; }
+        public EmailSender(IConfiguration _config)
+        {
+            SendGridSecret = _config.GetValue<string>("SendGrid:SecretKey");
+            SendGridEmail = _config.GetValue<string>("SendGrid:Email");
+        }
+
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            var emailToSend = new MimeMessage();
-            emailToSend.From.Add(MailboxAddress.Parse("abhipatel7420@gmail.com"));
-            emailToSend.To.Add(MailboxAddress.Parse(email));
-            emailToSend.Subject = subject;
-            emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
+            //var emailToSend = new MimeMessage();
+            //emailToSend.From.Add(MailboxAddress.Parse("abhipatel7420@gmail.com"));
+            //emailToSend.To.Add(MailboxAddress.Parse(email));
+            //emailToSend.Subject = subject;
+            //emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
 
-            //send email
-            using (var emailClient = new SmtpClient())
-            {
-                emailClient.Connect("smtp.mail.yahoo.com", 4565, MailKit.Security.SecureSocketOptions.StartTls);
-                emailClient.Authenticate("abhipatel7420@yahoo.com", "E:T3kn+Q'Cn - wKJ");
-                emailClient.Send(emailToSend);
-                emailClient.Disconnect(true);
-            }
+            ////send email
+            //using (var emailClient = new SmtpClient())
+            //{
+            //    emailClient.Connect("smtp.mail.yahoo.com", 4565, MailKit.Security.SecureSocketOptions.StartTls);
+            //    emailClient.Authenticate("email", "password");
+            //    emailClient.Send(emailToSend);
+            //    emailClient.Disconnect(true);
+            //}
 
-            return Task.CompletedTask;
+            var client = new SendGridClient(SendGridSecret);
+            var from = new EmailAddress("abhishek.patel@techlicious.in", "Bulky Book");
+            var to = new EmailAddress(email);
+            var message = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+            return client.SendEmailAsync(message);
         }
     }
 }
