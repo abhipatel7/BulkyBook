91b4017fcb379daeeefb15b6fb7c018e65eca8df
Abhishek Patel <abhipatel7420@gmail.com>
2022-11-01 15:47:34 +0530

    cleanup registration page code

--- BulkyBookWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BulkyBookWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -205,7 +205,14 @@ namespace BulkyBookWeb.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if(User.IsInRole(StaticDetails.Role_Admin)) 
+                        {
+                            TempData["success"] = "New User Created Successfully";
+                        } 
+                        else 
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                        }
                         return LocalRedirect(returnUrl);
                     }
                 }
